21ed25a021661b2bc9f94cab8306902c24a66d08
ChaiyoKung <kananek45@gmail.com>
2025-02-07 21:50:45 +0700

    feat: interest list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,66 @@
 import Image from "next/image";
 import ChaiyoKungWhiteLogo from "./chaiyokung-white-logo.svg";
+import { Agbalumo, B612_Mono, Cormorant } from "next/font/google";
+import { NextFont } from "next/dist/compiled/@next/font";
+import { ComponentProps } from "react";
+
+const b612Mono = B612_Mono({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+const agbalumo = Agbalumo({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+const cormorant = Cormorant({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+
+function VerticalLine() {
+  return <div className="bg-white h-full w-[1px] absolute bottom-[14px] opacity-50"></div>;
+}
+
+function InterestListItem({
+  label,
+  font,
+  className,
+}: {
+  label: string;
+  font: NextFont;
+  className?: ComponentProps<"span">["className"];
+}) {
+  return (
+    <li className="flex items-center gap-1">
+      <span className={`${font.className} text-white ${className}`}>{label}</span>
+      <span className="bg-white h-[1px] w-3 opacity-50 mr-[1px]"></span>
+    </li>
+  );
+}
+
+function ListItemCode() {
+  return <InterestListItem label="Code" font={b612Mono} className="text-sm md:text-base" />;
+}
+
+function ListItemDesign() {
+  return <InterestListItem label="Design" font={agbalumo} className="text-base md:text-lg" />;
+}
+
+function ListItemMusic() {
+  return <InterestListItem label="Music" font={cormorant} className="text-lg md:text-xl" />;
+}
+
+function InterestList() {
+  return (
+    <ul className="flex flex-col justify-end items-end">
+      <ListItemCode />
+      <ListItemDesign />
+      <ListItemMusic />
+    </ul>
+  );
+}
 
 function Logo() {
   return (
@@ -28,7 +89,11 @@ function Hero() {
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center min-h-screen p-4">
+    <main className="flex justify-center items-center min-h-screen p-4 relative">
+      <div className="container absolute flex flex-col justify-end items-end h-[40vh] top-0 w-[80%] max-w-screen-xl">
+        <VerticalLine />
+        <InterestList />
+      </div>
       <div className="flex flex-col lg:flex-row justify-center items-center gap-8">
         <Logo />
         <Divider />
